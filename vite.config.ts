36221bb0e5be98507cc605821dbc6fd9f53f33c8
semiexp <linthya.v@gmail.com>
2025-01-03 10:59:33 +0900

    change output file name

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,22 @@ export default defineConfig({
   plugins: [react(), comlink()],
   worker: {
     plugins: () => [comlink()],
-  }
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
+  base: "./",
 })
